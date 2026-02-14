5063bdc811c1b9da2cbdac92a0f3901afd1f80cd
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2026-02-14 16:17:01 +0900

    🎨変数の変更

--- preload.js
+++ preload.js
@@ -2,6 +2,6 @@ console.log("🚀 PRELOAD SCRIPT RUNNING");
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  saveData: (data) => ipcRenderer.send("save-data", data),
-  loadData: () => ipcRenderer.invoke("load-data"),
+  writeFile: (data) => ipcRenderer.send("save-data", data),
+  readFile: () => ipcRenderer.invoke("load-data"),
 });
